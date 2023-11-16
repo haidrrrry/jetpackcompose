31acdbec1d18f3ac3861f0d4998021142681491c
BasitMughal <basitullahkhan334@gmail.com>
2023-11-16 23:48:27 +0500

    ere

--- app/src/main/java/get/instagram/followers/jetpackcompose/ui/EditorHomeScreen.kt
+++ app/src/main/java/get/instagram/followers/jetpackcompose/ui/EditorHomeScreen.kt
@@ -1,6 +1,9 @@
 package get.instagram.followers.jetpackcompose.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.foundation.layout.Column
@@ -9,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
@@ -21,8 +25,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
 
 @Composable
@@ -65,31 +76,40 @@ fun CustomTextSliderSection(text:String)
     var textSize by remember {
         mutableStateOf(10.sp)
     }
+    var offset by remember { mutableStateOf(Offset(0f, 0f)) }
+
     var textOpacity by remember {
         mutableStateOf(1f)
     }
     var textsizeVisbility by remember {
         mutableStateOf(true)
     }
+    var FontVisbility by remember {
+        mutableStateOf(false)
+    }
  var textOpacityVisbility by remember {
         mutableStateOf(false)
     }
 
+    var fontFamily by remember { mutableStateOf(FontFamily.Default) }
+
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier
             .fillMaxWidth()
-            .align(Alignment.BottomCenter)
+            .offset {
+                IntOffset(offset.x.roundToInt(), offset.y.roundToInt())
+            }
+                .align(Alignment.BottomCenter)
             .padding(16.dp)
         ) {
 
-            Text(text = text, fontSize = textSize,
+            Text(text = text, fontSize = textSize,fontFamily=fontFamily,
                 modifier = Modifier
                     .fillMaxWidth()
                     .alpha(textOpacity)
                     .padding(16.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
-
             if (textsizeVisbility) {
                 Slider(value = textSize.value, onValueChange = {
                     textSize=it.sp
@@ -101,13 +121,6 @@ fun CustomTextSliderSection(text:String)
                         .padding(16.dp)
                 )
             }
-            Button(onClick = {
-                textsizeVisbility = true
-                textOpacityVisbility = false
-            }) {
-                Text(text = "size")
-            }
-
             Spacer(modifier = Modifier.height(16.dp))
 
             if (textOpacityVisbility) {
@@ -120,16 +133,85 @@ fun CustomTextSliderSection(text:String)
                         .padding(16.dp))
             }
 
+            if (FontVisbility)
+            {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.Bottom
 
-            Button(onClick = {
-                textsizeVisbility = false
-                textOpacityVisbility = true
-            }) {
-                Text(text = "Opacity")
+                ) {
+                    Button(
+                        onClick = { fontFamily = FontFamily.Cursive },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Cursive")
+                    }
+
+                    Button(
+                        onClick = { fontFamily = FontFamily.SansSerif },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Sans Serif")
+                    }
+
+                    Button(
+                        onClick = { fontFamily = FontFamily.Monospace },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Monospace")
+                    }
+
+                    Text(
+                        text = text,
+                        fontFamily = fontFamily,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)) {
+
+                Button(onClick = {
+                    textsizeVisbility = false
+                    textOpacityVisbility = true
+                    FontVisbility = false
+                }) {
+                    Text(text = "Opacity")
+                }
+
+                Button(onClick = {
+                    textsizeVisbility = true
+                    textOpacityVisbility = false
+                    FontVisbility = false
+                }) {
+                    Text(text = "size")
+                }
+                Button(onClick = {  textsizeVisbility = false
+                    textOpacityVisbility = false
+                    FontVisbility = true}) {
+                     Text(text = "FontFamily")
+                }
             }
             Spacer(modifier = Modifier.height(16.dp))
         }
     }
 
+}
+
+@Composable
+fun DynamicText(text: String, isSelected: Boolean, onTextClick: () -> Unit) {
+    Text(
+        text = text,
+        fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+        color = if (isSelected) Color.Red else Color.Black,
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { onTextClick() }
+    )
 }
 
